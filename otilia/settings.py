dd7e1d3724d02a74fe19f5c66716f7b3cc76d92c
mrescorcia <compumarana@gmail.com>
2021-06-16 22:00:55 -0500

    10:00

--- otilia/settings.py
+++ otilia/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-co'
+LANGUAGE_CODE = 'es-colombia'
 
 TIME_ZONE = 'UTC'
 
